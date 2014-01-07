f086e0ebe32e19683500ae5dd90756deeaec0585
John Otto <john.sinclair.otto@gmail.com>
2014-01-07 17:28:21 -0600

    added setup.py file using setuptools to build and install as a package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+    name="Wink",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=['httplib2'],
+    author="John S. Otto",
+    author_email="me@johnotto.net",
+    description="Library for interfacing with Wink devices by Quirky",
+    license="MIT",
+    keywords="Wink Quirky Nimbus Spotter Porkfolio Pivot Power Genius Eggminder",
+    url="https://github.com/jso/py-wink",
+)
