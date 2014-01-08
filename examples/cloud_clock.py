a17a111534070157b939b35b59ed29157ed34fa0
John Otto <john.sinclair.otto@gmail.com>
2014-01-08 07:36:48 -0600

    'show-off' example script of cloud_clock functionality.

---
+++ examples/cloud_clock.py
@@ -0,0 +1,30 @@
+if __name__ == "__main__":
+    import time
+    try:
+        import wink
+    except ImportError as e:
+        import sys
+        sys.path.insert(0, "..")
+        import wink
+
+    w = wink.init(config_file="../config.cfg")
+
+    if "cloud_clock" not in w.device_types():
+        raise RuntimeError("you do not have a cloud_clock associated with your account!")
+
+    c = w.cloud_clock()
+
+    print "found cloud_clock %s called %s!" % (c.id, c.data.get("name"))
+
+    print "'demoing' each of the dials:"
+
+    for i, dial in enumerate(c.dials):
+        print "dial #%d '%s'..." % (i+1, dial.data.get("name"))
+        dial.demo()
+
+    print "let's switch things up... rotate left!"
+    c.rotate()
+    time.sleep(5)
+
+    print "reverting to original state."
+    c.revert()
